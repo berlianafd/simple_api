e0a9506b77913172c268de5416fdd788166c0efc
Ilmiyatus <42805955+Ilmiyatus@users.noreply.github.com>
2020-04-09 14:12:35 +0700

    Update accounts.php

--- Admin/accounts.php
+++ Admin/accounts.php
@@ -51,138 +51,144 @@ $notelp = $admin['notelp'];
 <body id="reportsPage">
   <div class="" id="home">
     <nav class="navbar navbar-expand-xl">
-      <div class="container h-100">
-        <a class="navbar-brand" href="index-admin.php">
-          <h1 class="tm-site-title mb-0">POLINEMA-PAY</h1>
-        </a>
-        <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa-bars tm-nav-icon"></i>
-      </button>
+            <div class="container h-100">
+                <a class="navbar-brand" href="index-admin.php">
+                    <h1 class="tm-site-title mb-0">POLINEMA-PAY</h1>
+                </a>
+                <button class="navbar-toggler ml-auto mr-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars tm-nav-icon"></i>
+            </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mx-auto h-100">
-          <li class="nav-item">
-            <a class="nav-link" href="index-admin.php">
-              <i class="fas fa-home"></i>
-              Dashboard
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-users"></i>
-            <span>
-              User Mahasiswa <i class="fas fa-angle-down"></i>
-            </span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-           <a class="dropdown-item" href="peninjaupesanan.php">Peninjau Pesanan</a>
-           <a class="dropdown-item" href="hargasampah.php">Harga Sampah</a>
-         </div>
-       </li>
-       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-        aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-store"></i>
-        <span>
-          Merchant<i class="fas fa-angle-down"></i>
-        </span>
-      </a>
-      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="#">Tambah Merchant</a>
-        <a class="dropdown-item" href="#">Penukaran Poin</a>
-      </div>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="konversipoin.php">
-        <i class="fas fa-coins"></i>
-        Konversi Poin
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link active" href="accounts.php">
-        <i class="far fa-user"></i>
-        Profil
-      </a>
-    </li>
-    
-  </ul>
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link d-block" href="logout.php">
-        Admin, <b>Logout</b>
-      </a>
-    </li>
-  </ul>
-</div>
-</div>
-</nav>
-<div class="container mt-5">
-  <div class="row tm-content-row">
-    <div class="tm-block-col tm-col-avatar">
-      <div class="tm-bg-primary-dark tm-block tm-block-avatar">
-        <h2 class="tm-block-title">Foto</h2>
-        <div class="tm-avatar-container">
-          <img
-          src="img/user.png"
-          class="tm-avatar img-fluid mb-4"
-          />
-          <a href="#" class="tm-avatar-delete-link">
-            <i class="far fa-trash-alt tm-product-delete-icon"></i>
-          </a>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto h-100">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index-admin.php">
+                            <i class="fas fa-home"></i>
+                            Dashboard
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="peninjaupesanan.php">
+                            <i class="fas fa-clipboard-list"></i>
+                            Pesanan
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="konversiharga.php">
+                            <i class="fas fa-money-check-alt"></i>
+                            Konversi Harga
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="riwayat.php">
+                            <i class="fas fa-history"></i>
+                            Riwayat
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="accounts.php">
+                            <i class="far fa-user"></i>
+                            Profil
+                        </a>
+                    </li>
+
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link d-block" href="logout.php">
+                            Admin, <b>Logout</b>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <button class="btn btn-primary btn-block text-uppercase">
-          Upload foto
-        </button>
+
+    </nav>
+<div class="container tm-mt-big tm-mb-big">
+  <div class="row">
+    <div class="col-xl-9 col-lg-10 col-md-12 col-sm-12 mx-auto">
+      <div class="tm-bg-primary-dark tm-block tm-block-h-auto">
+        <div class="row">
+          <div class="col-12">
+            <center>
+            <h2 class="tm-block-title d-inline-block">Profil Admin</h2>
+            </center>
+          </div>
+        </div>
+        <div class="row tm-edit-product-row">
+          <div class="col-xl-6 col-lg-6 col-md-12">
+            <form action="" class="tm-edit-product-form">
+              <div class="form-group mb-3">
+                <label
+                for="name"
+                >Username
+              </label>
+              <input
+              id="username"
+              name="username"
+              type="text"
+              class="form-control validate"
+              value="<?php echo $admin['username']; ?>"
+              />
+            </div>
+            <div class="form-group mb-3">
+                <label
+                for="name"
+                >Password
+              </label>
+              <input
+              id="password"
+              name="password"
+              type="password"
+              class="form-control validate"
+              value="<?php echo $admin['password']; ?>"
+              />
+            </div>
+            <div class="form-group mb-3">
+                <label
+                for="name"
+                >No. Telepon
+              </label>
+              <input
+              id="notelp"
+              name="notelp"
+              type="text"
+              class="form-control validate"
+              value="<?php echo $admin['notelp']; ?>"
+              />
+            </div>
+      
+    </div>
+    <div class="col-xl-6 col-lg-6 col-md-12 mx-auto mb-4">
+      <div class="tm-product-img-dummy mx-auto">
+        <i
+        class="fas fa-cloud-upload-alt tm-upload-icon"
+        onclick="document.getElementById('fileInput').click();"
+        ></i>
+      </div>
+      <div class="custom-file mt-3 mb-3">
+        <input id="fileInput" type="file" style="display:none;" />
+        <input
+        type="button"
+        class="btn btn-primary btn-block mx-auto"
+        value="UNGGAH FOTO"
+        onclick="document.getElementById('fileInput').click();"
+        />
       </div>
     </div>
-    <div class="tm-block-col tm-col-account-settings">
-      <div class="tm-bg-primary-dark tm-block tm-block-settings">
-        <h2 class="tm-block-title">Profil</h2>
-        <form action="accounts-proses.php" class="tm-signup-form row">
-          <div class="form-group col-lg-6">
-            <label for="name">Username</label>
-            <input
-            id="name"
-            name="name"
-            type="text"
-            class="form-control validate"
-            value="<?php echo $admin['username']; ?>"
-            />
-          </div>
-          <div class="form-group col-lg-6">
-            <label for="password">Password</label>
-            <input
-            id="password"
-            name="password"
-            type="password"
-            class="form-control validate"
-            value="<?php echo $admin['password']; ?>"
-            />
-          </div>
-          <div class="form-group col-lg-6">
-            <label for="phone">Nomor Telepon</label>
-            <input
-            id="phone"
-            name="phone"
-            type="tel"
-            class="form-control validate"
-            value="<?php echo $admin['notelp']; ?>"
-            />
-          </div>
-          <div class="form-group col-lg-6">
-            <label class="tm-hide-sm">&nbsp;</label>
-            <button
-            type="simpan"
-            name="simpan"
-            class="btn btn-primary btn-block text-uppercase">
-            Update Profil
-          </button>
-        </div>
-    </form>
-  </div>
+    <div class="col-12">
+      <button type="simpan" name='simpan' class="btn btn-primary btn-block text-uppercase">SIMPAN</button>
+    </div>
+    <br><br><br>
+    <div class="col-12">
+      <button type="" class="btn btn-primary btn-block text-uppercase">Tambah Admin</button>
+    </div>
+  </form>
+</div>
+</div>
 </div>
 </div>
 </div>
