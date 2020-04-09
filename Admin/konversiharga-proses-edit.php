bb62c9a1adcdf9619dd4e0a25f5ec664f68c3edd
Ilmiyatus <42805955+Ilmiyatus@users.noreply.github.com>
2020-04-09 12:57:22 +0700

    Update and rename konversipoin-proses-edit.php to konversiharga-proses-edit.php

--- Admin/konversiharga-proses-edit.php
+++ Admin/konversiharga-proses-edit.php
@@ -6,17 +6,18 @@ require_once 'koneksi.php';
 if(isset($_POST['simpan'])){
 
     // ambil data dari formulir
-    $id = $_POST['id'];
+    $id_jenis = $_POST['id_jenis'];
+    $nama_jenis = $_POST['nama_jenis'];
     $poin = $_POST['poin'];
     $harga = $_POST['harga'];
 
     // buat query update
-    $query = mysqli_query($koneksi, "UPDATE konversi_poin SET poin='$poin', harga='$harga' WHERE id='$id'");
+    $query = mysqli_query($koneksi, "UPDATE konversiharga SET nama_jenis='$nama_jenis', harga='$harga', poin='$poin', WHERE id_jenis='$id_jenis'");
 
     // apakah query update berhasil?
     if( $query ) {
         // kalau berhasil alihkan ke halaman list-siswa.php
-        header('Location: konversipoin.php');
+        header('Location: konversiharga.php');
     } else {
         // kalau gagal tampilkan pesan
         die("Gagal menyimpan perubahan...");
